ba55c2ea2c2430f3cad0fa256176798d3a0e859c
Artyom Safronov <a.safronov@haulmont.net>
2024-06-27 10:21:48 +0400

    add select

---
+++ components/select/Select.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Select } from "./Select";
+import { Link } from "@mui/material";
+
+const meta: Meta<typeof Select> = {
+  title: "Select",
+  component: Select,
+  parameters: {
+    layout: "centered",
+    controls: {},
+  },
+  decorators: [],
+  args: {
+    options: [
+      { id: "First option", label: "First option" },
+      { id: "Second option", label: "Second option" },
+    ],
+    label: "Select option:",
+    link: (
+      <Link href={"javascript:void(0)"} underline="none" variant="caption">
+        link component
+      </Link>
+    ),
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Select>;
+
+export const Default: Story = {
+  render: ({ label, link, options }) => {
+    return <Select label={label} link={link} options={options} />;
+  },
+  args: {},
+};
---
+++ components/select/Select.tsx
@@ -0,0 +1,45 @@
+import {
+  Box,
+  FormControl,
+  MenuItem,
+  Select as MuiSelect,
+  OutlinedInput,
+  Stack,
+  Typography,
+} from "@mui/material";
+
+type Option = {
+  id: string;
+  label: string;
+}
+
+type SelectProps = {
+  label: string;
+  options: Option[];
+  link?: JSX.Element;
+};
+
+export const Select = ({ label, link, options }: SelectProps) => {
+  return (
+    <Box width={200}>
+      <Stack direction={"row"} justifyContent={"space-between"}>
+        <Typography variant="caption">{label}</Typography>
+        {link && link}
+      </Stack>
+      <FormControl sx={{ width: "100%" }}>
+        <MuiSelect
+          labelId="size-select-label"
+          defaultValue={options[0].id}
+          label="Age"
+          input={<OutlinedInput />}
+        >
+          {options.map((option) => {
+            return (
+              <MenuItem value={option.label}>{option.label}</MenuItem>
+            )
+          })}
+        </MuiSelect>
+      </FormControl>
+    </Box>
+  );
+};
